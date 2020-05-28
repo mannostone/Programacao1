4542f0d47e263661b18ca4b6609d2fce8db45004
Manno <si.jorgelucas@gmail.com>
2020-05-28 09:42:14 -0300

    testando edit com o vs code
    nada demais

--- test/app.java
+++ test/app.java
@@ -4,7 +4,7 @@ public class app {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("PQ nao ta marcando?");
+		System.out.println("Teste feito com sucesso?");
 	}
 
 }
